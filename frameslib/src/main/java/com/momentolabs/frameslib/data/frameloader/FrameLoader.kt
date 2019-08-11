1e0dba70886f50f71d039575349a975e9b439796
mertsimsek <mertsimsek1992@gmail.com>
2019-08-11 11:35:26 +0300

    Add frames art.

--- frameslib/src/main/java/com/momentolabs/frameslib/data/frameloader/FrameLoader.kt
+++ frameslib/src/main/java/com/momentolabs/frameslib/data/frameloader/FrameLoader.kt
@@ -4,7 +4,7 @@ import com.momentolabs.frameslib.data.model.FrameRetrieveRequest
 import com.momentolabs.frameslib.data.model.FramesResource
 import io.reactivex.Observable
 
-interface FrameLoader<in T : FrameRetrieveRequest> {
+interface FrameLoader<T : FrameRetrieveRequest> {
 
     fun loadFrames(frameRetrieveRequest: T): Observable<FramesResource>
 }
